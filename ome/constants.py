456bb674fc7429b4cee6b7c901c39716e0aa415f
Luke McCarthy <luke@iogopro.co.uk>
2015-12-02 23:18:23 +0000

    Reserve tag of all 1's for untagged negative integers.

--- ome/constants.py
+++ ome/constants.py
@@ -10,9 +10,11 @@ NUM_SIGNIFICAND_BITS = NUM_DATA_BITS - NUM_EXPONENT_BITS
 GC_SIZE_BITS = 8  # Maximum object size 2^8-1 = 255 slots (~2 KB)
 GC_SIZE_MASK = (1 << GC_SIZE_BITS) - 1
 
-MAX_TAG = 2**(NUM_TAG_BITS-1) - 1  # Highest bit of tag is error bit
+# Tag with all 1 bits is reserved (for untagged negative integers)
+MAX_TAG = 2**(NUM_TAG_BITS-1) - 2  # Highest bit of tag is error bit
 MIN_CONSTANT_TAG = 2**NUM_TAG_BITS
 MAX_CONSTANT_TAG = 2**32 - MIN_CONSTANT_TAG - 1
+
 MIN_INT = -2**(NUM_DATA_BITS-1)
 MAX_INT = 2**(NUM_DATA_BITS-1) - 1
 MIN_EXPONENT = -2**(NUM_EXPONENT_BITS-1)
