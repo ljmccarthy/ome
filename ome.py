b1120dca4d0efce9a03a5db2ee9f7c9c7c386b6a
Luke McCarthy <luke@iogopro.co.uk>
2015-12-03 18:26:27 +0000

    Check Python version.

--- ome.py
+++ ome.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+#
 # ome - Object Message Expressions
 # Copyright (c) 2015 Luke McCarthy <luke@iogopro.co.uk>. All rights reserved.
 
@@ -6,13 +8,14 @@ from ome.compiler import compile_file, Error
 
 def main():
     if len(sys.argv) != 2:
-        sys.stderr.write('usage: python ome.py <infile.ome>\n')
+        sys.exit('usage: python ome.py <infile.ome>')
     else:
         try:
             compile_file(sys.argv[1])
         except Error as e:
-            sys.stderr.write('%s\n' % e)
-            sys.exit(1)
+            sys.exit(str(e))
 
 if __name__ == '__main__':
+    if sys.version_info[0] < 3:
+        sys.exit('Please run in Python 3')
     main()
