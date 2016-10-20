f4e9669f9e4aaf79ff46bc12426ef7c142cdd78c
Luke McCarthy <luke@iogopro.co.uk>
2016-10-20 02:04:48 +0100

    Moved main into ome module.

--- ome/__main__.py
+++ ome/__main__.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python3
-#
 # ome - Object Message Expressions
 # Copyright (c) 2015-2016 Luke McCarthy <luke@iogopro.co.uk>. All rights reserved.
 
 import sys
-from ome.compiler import compile_file, OmeError
 
 def main():
+    from .compiler import compile_file, OmeError
+
     if len(sys.argv) != 2:
-        sys.exit('usage: python ome.py <infile.ome>')
+        sys.exit('usage: ome <infile.ome>')
     else:
         try:
             compile_file(sys.argv[1])
@@ -17,5 +16,5 @@ def main():
 
 if __name__ == '__main__':
     if sys.version_info[0] < 3:
-        sys.exit('Please run in Python 3')
+        sys.exit('Please use Python 3')
     main()
