bab6568060892385a8bfc24251f13d2786c72da5
Luke McCarthy <luke@iogopro.co.uk>
2016-11-30 23:43:14 +0000

    Added libtommath.

--- ome/target/lang_c/packages.py
+++ ome/target/lang_c/packages.py
@@ -1,16 +1,25 @@
-import subprocess
 from os.path import join
 from ...package import SourcePackage
 
-def build_mpdecimal(shell, backend, options):
+def get_cflags(backend):
     cflags = ['-DNDEBUG', '-O3', '-fomit-frame-pointer', '-fPIC']
     if backend.name == 'clang':
         cflags.append('-Qunused-arguments')
-    shell.run('./configure', 'CC=' + backend.command, 'CFLAGS=' + ' '.join(cflags))
+    return cflags
+
+def build_mpdecimal(shell, backend, options):
+    shell.run('./configure', 'CC=' + backend.command, 'CFLAGS=' + ' '.join(get_cflags(backend)))
     shell.cd('libmpdec')
     shell.run('make', 'libmpdec.a')
     shell.copy('mpdecimal.h', options.include_dir)
     shell.copy('libmpdec.a', options.library_dir)
+
+def build_libtommath(shell, backend, options):
+    shell.run('make', 'CC=' + backend.command, 'CFLAGS=' + ' '.join(get_cflags(backend) + ['-I.']))
+    shell.copy('tommath.h', options.include_dir)
+    shell.copy('tommath_class.h', options.include_dir)
+    shell.copy('tommath_superclass.h', options.include_dir)
+    shell.copy('libtommath.a', options.library_dir)
 
 packages = [
     SourcePackage(
@@ -22,6 +31,19 @@ packages = [
         output_files = [
             join('include', 'mpdecimal.h'),
             join('lib', 'libmpdec.a'),
+        ]
+    ),
+    SourcePackage(
+        name = 'libtommath',
+        version = '1.0',
+        hash = '993a7df9ee091fca430cdde3263df57d88ef62af8103903214da49fc51bbb56c',
+        url = 'https://github.com/libtom/libtommath/releases/download/v{version}/ltm-{version}.tar.xz',
+        build = build_libtommath,
+        output_files = [
+            join('include', 'tommath.h'),
+            join('include', 'tommath_class.h'),
+            join('include', 'tommath_superclass.h'),
+            join('lib', 'libtommath.a'),
         ]
     ),
 ]
